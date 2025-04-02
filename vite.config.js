ee6e35ff0fc471f00583b1db8c7b81c64faeb81c
Benjamin <gracielvelazquez464@gmail.com>
2025-04-01 17:02:21 -0700

    cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GIF-EXPERT-APP/', // 👈 Agrega esto para que los recursos se carguen correctamente en GitHub Pages
 })
